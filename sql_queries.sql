53596c913406a0a2398c2a6d77bfc232af7e6475
fantahunteka1980 <fantutero@gmail.com>
2021-06-17 21:02:05 -0500

    cleaned the tables and created tables on pgAdmin

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,2 +1,9 @@
 select *
 from chlamydia_age_group_and_sex_2019;
+select year, max(population) as max_population 
+from chlamydia_age_group_and_sex_2019
+group by year;
+
+
+select *
+from chlamydia_males_aged_15_44_years;
